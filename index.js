0d54aa0b9ae93467c63de67775f10132fd4d48bd
Alex McComb <alex@impossible-bureau.com>
2020-11-24 00:08:23 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const startIntersectionObserver = () => {
+    const features = document.querySelectorAll('.feature');
+
+    const observer = new IntersectionObserver(sections => {
+        sections.forEach(section => {
+            if (section.intersectionRatio > 0.5) {
+                section.target.classList.add('show-feature');
+            } else {
+                section.target.classList.remove('show-feature');
+            }
+        });
+    }, { threshold: 0.5 });
+
+    features.forEach(feature => observer.observe(feature))
+}
+
+startIntersectionObserver();
+
